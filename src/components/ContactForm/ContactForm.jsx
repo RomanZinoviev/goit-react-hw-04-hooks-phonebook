33a1e517bf468110748ccfdce002c0da9dcbd582
RomanZinoviev <rzinovijev@gmail.com>
2022-05-19 16:26:41 +0300

    done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,31 +1,28 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import s from "../ContactForm/ContactForm.module.css"
 
-export class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export function ContactForm ({array, onSubmit}) {
+  
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  
+  const reset = () => {
+    setName("");
+    setNumber("")
   };
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-    array: PropTypes.arrayOf(PropTypes.object).isRequired
+  const hundleChangeForName = event => {
+    const { value } = event.target;    
+    setName(value);
   };
-  reset() {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  }
-  hundleChange = event => {
-    const { name, value } = event.target;    
-    this.setState({ [name]: value });
+  const hundleChangeForNumber = event => {
+    const { value } = event.target;    
+    setNumber(value);
   };
-  hundleSubmit = event => {
-    const { name, number } = this.state;
+  const hundleSubmit = event => {
     event.preventDefault();
-    const sameName = this.props.array.find(arr => arr.name === name);
+    const sameName = array.find(arr => arr.name === name);
     if (sameName) {
       return alert(`${name} is already in contacts`);
     }
@@ -34,19 +31,17 @@ export class ContactForm extends Component {
       name: name,
       number: number,
     };
-    this.props.onSubmit(nameObj);
-    this.reset();
+    onSubmit(nameObj);
+    reset();
   };
-  render() {
-    const { name, number } = this.state;
-    return (
+      return (
       <form className={s.form}>
         <label className={s.label}>
           Name
           <input
             className={s.input}
             value={name}
-            onChange={this.hundleChange}
+            onChange={hundleChangeForName}
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -60,19 +55,22 @@ export class ContactForm extends Component {
             className={s.input}
             type="tel"
             value={number}
-            onChange={this.hundleChange}
+            onChange={hundleChangeForNumber}
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
         </label>
-        <button type="submit" onClick={this.hundleSubmit} className={s.button} >
+        <button type="submit" onClick={hundleSubmit} className={s.button} >
           Add contact
         </button>
       </form>
     );
-  }
-};
+  };
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    array: PropTypes.arrayOf(PropTypes.object).isRequired
+  };
 
 
